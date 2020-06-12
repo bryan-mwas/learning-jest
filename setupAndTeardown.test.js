4403a14f44cc68783a174d2454df6fca7f3208fc
Brian Mwathi <mwathibrian7@gmail.com>
2020-06-12 13:07:13 +0300

    Mock up database
    * Confirm it works on the CLI

--- setupAndTeardown.test.js
+++ setupAndTeardown.test.js
@@ -1,14 +1,44 @@
-beforeAll(() => console.log('1 - beforeAll'))
-afterAll(() => console.log('1 - afterAll'))
-beforeEach(() => console.log('1 - beforeEach'))
-afterEach(() => console.log('1 - afterEach'))
+let cities = [];
 
-test('', () => console.log('1 - test'))
+function initializeCityDatabase() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      cities.push("Vienna");
+      cities.push("San Juan");
+      resolve();
+    }, 1000);
+  });
+}
 
-describe('Scope/Nested block', () => {
-  beforeAll(() => console.log('2 - beforeAll'))
-  afterAll(() => console.log('2 - afterAll'))
-  beforeEach(() => console.log('2 - beforeEach'))
-  afterEach(() => console.log('2 - afterEach'))
-  test(() => console.log('2 - test'))
-})
+function clearCityDatabase() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      cities = [];
+      resolve();
+    }, 2000);
+  });
+}
+
+function isCity(name) {
+  return cities.includes(name);
+}
+
+beforeEach(() => {
+  return initializeCityDatabase();
+});
+
+afterEach(() => {
+  clearCityDatabase();
+});
+
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
+
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
+
+test("database has only 2 cities", () => {
+  expect(cities.length).toBe(2);
+});
